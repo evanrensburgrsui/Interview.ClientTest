03ed89e24cdc61884b23fbafe4261b6ea0c94a60
Van Rensburg <evanrensburg@rsui.com>
2022-07-28 10:57:53 -0400

    fixed title required error

--- tools/mockServer.js
+++ tools/mockServer.js
@@ -20,33 +20,9 @@ server.use((req, res, next) => {
   next();
 });
 
-server.post("/tasks/", function(req, res, next) {
-  const error = validateCourse(req.body);
-  if (error) {
-    res.status(400).send(error);
-  } else {
-    req.body.slug = createSlug(req.body.title);
-    next();
-  }
-});
-
 server.use(router);
 
 const port = 3001;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
-
-function createSlug(value) {
-  return value
-    .replace(/[^a-z0-9_]+/gi, "-")
-    .replace(/^-|-$/g, "")
-    .toLowerCase();
-}
-
-function validateCourse(course) {
-  if (!course.title) return "Title is required.";
-  if (!course.authorId) return "Author is required.";
-  if (!course.category) return "Category is required.";
-  return "";
-}
